97b5ef765b8a7a9e8834265f79fcb1b38a752c30
Luis Alonso <luisalonsoortiz343@gmail.com>
2026-05-13 14:49:11 -0700

    Creacion del modulo de negocio

---
+++ Negocio/src/main/java/com/mycompany/negocio/Negocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.negocio;
+
+/**
+ *
+ * @author Central
+ */
+public class Negocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
